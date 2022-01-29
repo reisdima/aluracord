6e041bc809fcb9f0f56fc5e0b401ed1e6a38f8fc
Caio <caio.vincenzo@estudante.ufjf.br>
2022-01-29 19:34:10 -0300

    Segunda e terceira aula

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router'
+import React from 'react';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -46,22 +20,13 @@ function Title(props) {
     );
 }
 
-// function HomePage() {
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Title tag="h1">Boas vindas de volta!</Title>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     );
-// }
-
 export default function PaginaInicial() {
-    const username = 'reisdima';
+    // const username = 'reisdima';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -88,6 +53,10 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (event) {
+                            event.preventDefault();
+                            roteamento.push('/chat')
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,6 +69,11 @@ export default function PaginaInicial() {
 
                         <TextField
                             fullWidth
+                            value={username} type="text"
+                            onChange={function (event) {
+                                const valor = event.target.value;
+                                setUsername(valor)
+                            }}
                             textFieldColors={{
                                 neutral: {
                                     textColor: appConfig.theme.colors.neutrals[200],
